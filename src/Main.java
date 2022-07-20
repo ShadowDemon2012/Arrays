99dc4338c73f5ff24689a988c4d29758ced6e9ea
Dmitriy <shadowgods@mail.ru>
2022-07-21 02:19:39 +0500

    Четвертая задача

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,21 @@ public class Main {
     public static void main(String[] args) {
         int[] firstArray = new int[3];
         firstArray[0] = 1;
-        firstArray[1] = 2;
-        firstArray[2] = 4;
-        double[] secondArray = {1.57, 7.564, 9.986};
-        char[] thirdArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g' };
-        for (int i = firstArray.length; i > 0; i--) {
-            System.out.print(firstArray[i - 1]);
-            if (i > 1) {
-                System.out.print(", ");
-            } else {
-                System.out.println();
-            }
-        }
-        for (int i = secondArray.length; i > 0; i--) {
-            System.out.print(secondArray[i - 1]);
-            if (i > 1) {
-                System.out.print(", ");
+        firstArray[1] = 4;
+        firstArray[2] = 9;
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] % 2 != 0) {
+                System.out.print(firstArray[i] + 1);
+            } else System.out.print(firstArray[i]);
+            if (i < firstArray.length - 1) {
+                System.out.print(",");
             } else System.out.println();
         }
-        for (int i = thirdArray.length; i > 0; i--) {
-            System.out.print(thirdArray[i - 1]);
-            if (i > 1) {
-                System.out.print(", ");
-            } else System.out.println();
-        }
-
     }
 }
+
+
+
 
 
 
