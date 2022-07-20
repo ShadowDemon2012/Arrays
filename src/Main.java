f70545483361a964ad959b6ed7cfb5a170ecd27e
Dmitriy <shadowgods@mail.ru>
2022-07-21 00:53:55 +0500

    Первая задача

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] firstArray = new int[3];
+        firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 4;
+        double[] secondArray = {1.57, 7.564, 9.986};
+        char[] thirdArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g' };
     }
 }
 
