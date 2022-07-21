df9d3f1111de92bb7739d25d6cd806335f84cb29
Dmitriy <shadowgods@mail.ru>
2022-07-21 20:46:35 +0500

    Четвертая задача(исправление)

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         firstArray[2] = 9;
         for (int i = 0; i < firstArray.length; i++) {
             if (firstArray[i] % 2 != 0) {
-                System.out.print(firstArray[i] + 1);
-            } else System.out.print(firstArray[i]);
+                firstArray[i]++;}
+                 System.out.print(firstArray[i]);
             if (i < firstArray.length - 1) {
                 System.out.print(",");
             } else System.out.println();
